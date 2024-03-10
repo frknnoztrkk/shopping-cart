2ccc8b6be31eed1f2f9a772b289d0f4ae6df210d
Furkan Öztürk <furkan25ozturk@gmail.com>
2024-03-10 12:59:35 +0300

    ContextApi,useContext

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -1,13 +1,18 @@
 import { FiShoppingCart } from "react-icons/fi";
+import { MyContext } from "../main";
+import { useContext } from "react";
 
 const CartIcon = ({ cart }) => {
+  const { myState, setMyState } = useContext(MyContext);
+
   return (
     <div className="relative">
-      <FiShoppingCart className="text-2xl" />
+      <FiShoppingCart className="text-2xl" onClick={() => setMyState("Emir")} />
+      {myState}
       {cart.length > 0 && (
         <span className="bg-red-500 text-white w-5 h-5 flex justify-center items-center rounded-full absolute -top-2 -right-4 text-xs">
-        {cart.length}
-      </span>
+          {cart.length}
+        </span>
       )}
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+export const MyContext = React.createContext();
+
+const MyProvider = ({ children }) => {
+  const [myState, setMyState] = useState("Furkan");
+
+  const value ={
+    myState,
+    setMyState,
+  }
+
+  return (
+    <MyContext.Provider
+      value={value}
+    >
+      {children}
+    </MyContext.Provider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MyProvider>
+      <App />
+    </MyProvider>
+  </React.StrictMode>
+);
